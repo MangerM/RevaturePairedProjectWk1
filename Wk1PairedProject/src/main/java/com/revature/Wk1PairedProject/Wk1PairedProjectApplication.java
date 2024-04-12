a21f1fba8c567c6abbde68d955eedc9546e17427
MangerM <mangerml1891@gmail.com>
2024-04-12 17:00:21 -0400

    initial commit to set up the project

---
+++ Wk1PairedProject/src/main/java/com/revature/Wk1PairedProject/Wk1PairedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Wk1PairedProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk1PairedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wk1PairedProjectApplication.class, args);
+	}
+
+}
---
+++ Wk1PairedProject/src/test/java/com/revature/Wk1PairedProject/Wk1PairedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Wk1PairedProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk1PairedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
